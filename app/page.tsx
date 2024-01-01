c27dc8ea473e06a3d1f4e152eee5f782b72c6274
Seemo0 <mouallal@valhko.com>
2024-01-01 19:49:18 +0100

    adding the checkoutPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-
+import CheckoutPage from "@/components/CheckoutPage";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+    <main className="flex min-h-screen flex-col bg-slate-500 items-center justify-between p-24">
+      <CheckoutPage />
     </main>
   );
 }
